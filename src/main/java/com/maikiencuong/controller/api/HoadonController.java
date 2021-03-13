b7e73c767221b078aa0cd48f2f2aa6b7a2f173e6
Mai Kien Cuong <maikiencuongiuh@gmail.com>
2021-03-13 22:36:09 +0700

    them @CrossOrigin('*')

--- src/main/java/com/maikiencuong/controller/api/HoadonController.java
+++ src/main/java/com/maikiencuong/controller/api/HoadonController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import com.maikiencuong.jsonView.Views;
 import com.maikiencuong.service.HoadonService;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/api")
 public class HoadonController {
 
